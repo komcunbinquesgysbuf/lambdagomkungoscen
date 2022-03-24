ae6d5997971b06fc63552146085226cb3084c363
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-03-24 11:53:46 +0000

    Change app to produce JWT for zilbesveldoswinkos

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello World! ' + JSON.stringify(process.env.FOO))
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const express = require('express');
+const crypto = require('crypto').webcrypto;
+const createJsonWebToken = async (payload, secret) => await (
+    async (stringToSign, key) => [
+        stringToSign,
+        Buffer.from(await crypto.subtle.sign('HMAC', key, Buffer.from(stringToSign, 'binary'))).toString('base64url')
+    ].join('.')
+)(
+    [{alg: 'HS256', typ: 'JWT'}, {iat: (new Date().getTime() / 1000).toFixed(), ...payload}]
+        .map(j => JSON.stringify(j)).map(t => Buffer.from(t, 'binary').toString('base64url')).join('.'),
+    await crypto.subtle.importKey('raw', Buffer.from(secret, 'binary'), {name: 'HMAC', hash: 'SHA-256'}, true, ['sign'])
+);
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+app.post('/zilbesveldoswinkos/jwt', async (req, res) => res
+    .contentType('text/plain')
+    .send(await createJsonWebToken(req.body, process.env.ZILBESVELDOSWINKOS_JWT_SECRET))
+);
+app.listen(parseInt(process.env.PORT, 10) || 3000);
