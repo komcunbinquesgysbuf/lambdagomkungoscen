4d7572651c585ea7f1090b601bcaf97318cb5561
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-03-24 09:56:08 +0000

    Add dotenv to app and test output

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! ' + JSON.stringify(process.env.FOO))
 })
 
 app.listen(port, () => {
