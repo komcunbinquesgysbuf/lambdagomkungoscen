fa37cfd729e0f222e13c839f9f12def8881fe0ec
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-03-29 23:24:58 +0000

    Change health check path to GET

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 require('dotenv').config();
 // app.use(express.json());
 // app.use(express.text());
-app.post('/healthz', (req, res) => res.status(200).end());
+app.get('/healthz', (req, res) => res.status(200).end());
 app.listen(parseInt(process.env.PORT, 10) || 3000);
